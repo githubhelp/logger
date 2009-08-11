2eec4a1f30ba7224f870ce824b643deac08e1eca
Simon Norberg <simon@dackbrann.net>
2009-08-11 15:00:39 +0200

    sensorLogger is now support all kinds of sensors not just temperature

--- readSensor.py
+++ readSensor.py
@@ -3,28 +3,31 @@ import serial, re
 def readSensor():
 	ser = serial.Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=3)
 	ser.open()
-	value = ser.readline()
+	input = ser.readline()
 	try:
-		sensor = re.split(" Sensor: | ", value)[1]
+		sensor = re.split(" Sensor: | ", input)[1]
 	except:
-		print "Incorretly formated:", value
+		#print "Incorretly formated:", input
 		return readSensor()
 		
 	if sensor == "Temp":
-		id = re.split("Sensor: | ID: | Temp: |C\r\n", value)[2]
-		temp = re.split("Sensor: | ID: | Temp: |C\r\n", value)[3]
+		id = re.split("Sensor: | ID: | Temp: |C\r\n", input)[2]
+		value = re.split("Sensor: | ID: | Temp: |C\r\n", input)[3]
 	elif sensor == "Error":
-		print value
+		print input
 	else:
-		print "Incorretly formated:", value
+		print "Incorretly formated:", input
 
 	ser.close()
 
-	return temp, id
+	return value, id, sensor 
 
 def main():
-	temp, id = readSensor()
-	print "ID:", id, "Temp: ", temp, "C"
+	value, id, sensor = readSensor()
+	if sensor == "Temp":
+		print "ID:", id, "Temp: ", value, "C"
+	else:
+		print "Sensor:", sensor, "ID:", id, "Value:", value
 
 if __name__ == "__main__":
 	main()
--- sensorLogger.py
+++ sensorLogger.py
@@ -1,7 +1,8 @@
-import sqlite3, sys, datetime
+import sqlite3, time, datetime
 import readSensor
 
 #settings
+loggTime = 5 # nr of minutes between logging to db
 sensorLookUp = {"10 DE C6 35 1 8 0 86" : "outside", \
 		"10 C4 EB 35 1 8 0 6" : "test"}
 
@@ -9,24 +10,48 @@ def logging():
 	date = datetime.datetime.now().strftime("%Y-%m-%d")
 	time = datetime.datetime.now().strftime("%H:%M")
 	sensorReadings = {}
+	readings = 0
 	while 1:
 		try:
-			temp, rom = getTemp.getTemp()
+			value, id, sensor = readSensor.readSensor()
 		except:
-			print "not able to get temp at", date, time
-			sys.exit()
-		if rom != "null":
-			sensorReadings[sensorLookUp[rom]] = temp
-		else:
-			break
+			print "not able to get sensor reading:", date, time
+			return False # unsuccesfully logged
+
+		try:
+			#if sensor allready have been read then we read them all
+			if sensorReadings.has_key(sensorLookUp[id]):
+				break
+
+			sensorReadings[sensorLookUp[id]] = (sensor, value)
+		except:
+			print "ID:", id, "not found in sensorLookUp, please add"
 		
 
-
-	conn = sqlite3.connect("/home/simon/tempLog/temp.db")
+	conn = sqlite3.connect("/home/simon/logger/sensors.db")
 	c = conn.cursor()
-	for sensor in sensorReadings:
-		c.execute("insert into temp values(?, ?, ?, ?)",\
-			 [date, time, sensorReadings[sensor], sensor])
+	for name in sensorReadings:
+		#date, time, value, sensor, name
+		c.execute("insert into sensors values(?, ?, ?, ?, ?)",\
+			 [date, time, sensorReadings[name][0], \
+			  sensorReadings[name][1], name])
 
 	conn.commit();
 	c.close()
+	print "Successfully written readings to db at:", date, time
+	return True # succesfully logged
+
+def main():
+	lastLoged = 0
+	while 1:
+		timetuple = datetime.datetime.now().timetuple()
+		minutesToday = timetuple[3] * 60 + timetuple[4]
+		#if its time to logg and not allready logged	
+		if minutesToday % loggTime == 0 and lastLoged != minutesToday:
+			lastLoged = minutesToday
+			logging()
+		time.sleep(30) # sleep 30 seconds	
+			
+
+if __name__ == "__main__":
+	main()
