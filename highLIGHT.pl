5116efd0c54ba865f2be5b11b7b66779c6b38741
Simon Norberg <simon@dackbrann.net>
2009-10-04 19:17:00 +0200

    irssi script to turn on LED on hightligt and priv msg

---
+++ highLIGHT.pl
@@ -0,0 +1,47 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use Irssi;
+$VERSION = '0.1';
+%IRSSI = (
+	authors     => 'Simon Norberg',
+	contact     => 'simon@dackbrann.net',
+	name        => 'highLIGTH',
+	description => 'Sends a signal to external hardware to light up on highligth and off on dehighligt',
+	license     => 'BSD',
+);
+
+my $highlighted = 0;
+
+sub highLIGHT 
+{
+	my ($dest) = @_;
+	$highlighted = 1;
+	if ($dest->{level} & MSGLEVEL_HILIGHT)
+	{
+		system("python /home/simon/logger/writeLED.py green on");
+	}
+}
+
+sub dehighLIGHT 
+{
+	if ($highlighted == 1)
+	{
+		$highlighted = 0;
+		system("python /home/simon/logger/writeLED.py green off");
+		system("python /home/simon/logger/writeLED.py blue off");
+	}
+}
+
+sub priv_msg
+{
+	system("python /home/simon/logger/writeLED.py blue on");
+}
+#--------------------------------------------------------------------
+# Irssi::signal_add_last / Irssi::command_bind
+#--------------------------------------------------------------------
+Irssi::signal_add_last("message private", "priv_msg");
+Irssi::signal_add_last("print text", "highLIGHT");
+Irssi::signal_add_last("window changed", "dehighLIGHT");
+
+#- end
