2032e8ce2b186f55e525a78e899f1911a7f1871e
Simon Norberg <simon@dackbrann.net>
2010-01-02 18:01:49 +0100

    now support handling LEDS with pwm usage: python writeLED.py white [0-255]

--- weekly.plot
+++ weekly.plot
@@ -9,6 +9,7 @@ set datafile separator "|"
 set style data lines
 set grid
 set yrange [0:*]
+set autoscale ymin
 set xdata time
 set timefmt x "%Y-%m-%d %H:%M"
 set format x "%m-%d"
--- writeLED.py
+++ writeLED.py
@@ -1,20 +1,35 @@
-import serial, sys
+import serial, sys, glob
+
+#returns first ttyUSB* port avalible
+def findPort():
+	return glob.glob("/dev/ttyUSB*")[0]
+
 def writeLED(led, status):
-	ser = serial.Serial(port="/dev/ttyUSB0",baudrate=9600,timeout=3)
+	ser = serial.Serial(port=findPort(),baudrate=9600,timeout=3)
 	if status == True:	
-		ser.write(led.upper()[0]);
+		ser.write(led.upper()[0])
 	elif status == False:
-		ser.write(led.lower()[0]);
+		ser.write(led.lower()[0])
 		
-	ser.close();
+	ser.close()
+
+def writeLED_PWM(led, level):
+	ser = serial.Serial(port=findPort(),baudrate=9600,timeout=3)
+	ser.write(led.lower()[0])
+	ser.write(chr(level))
+	ser.close()
+		
 def main():
 	try:
 		if (sys.argv[2] == "on"):
 			writeLED(sys.argv[1], True)
-		if (sys.argv[2] == "off"):
+		elif (sys.argv[2] == "off"):
 			writeLED(sys.argv[1], False)
+		elif (sys.argv[2].isdigit()):
+			writeLED_PWM(sys.argv[1], int(sys.argv[2]))
 	except:
-		print sys.argv
+		print "Error:", sys.exc_info()
+		print "Input:", sys.argv
 		pass
 
 if __name__ == "__main__":
